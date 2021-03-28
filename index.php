cfb6081fd4f05e30ced45016ae79cabc9b854318
Menno den Engelse <menno@denengelse.nl>
2021-03-28 10:11:06 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Het Wilde NL | <?= $year ?></title>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="assets/css/styles.css" />
+	
+</head>
+<body>
+
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>het wilde nl</h1>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+			<p class="lead">
+				Biodiversiteit, klimaat - alle zeilen moeten bij om de planeet een beetje leefbaar achter te laten voor 	komende generaties. Erfgoed stelt ons in staat iets verder terug te kijken dan ons <a href="https://en.wikipedia.org/wiki/Shifting_baseline">shifting baseline syndrome</a> lang is. En daar lessen voor de toekomst uit te trekken.
+			</p>
+
+			<p class="lead">
+				Dit is een persoonlijke inventarisatie van erfgoedprojecten die een bijdrage kunnen leveren aan en dingen die we zouden kunnen doen voor een groenere wereld. Activisme? De tijd is er rijp voor.
+			</p>
+
+			<div class="row">
+				<div class="col-md-6">
+
+
+					<h2>Topotijdreis</h2>
+					<p>
+						Het geweldige <a href="https://topotijdreis.nl/">Topotijdreis</a> toont anderhalve eeuw veranderingen in het landschap - niets is onveranderlijk en van alles mogelijk. Overal wordt woeste grond ontgonnen, water drooggelegd - en een heel enkel keertje andersom.
+					</p>
+
+					<img src="assets/img/naardermeer1885.jpg" />
+
+					<p class="onderschrift buildings">
+						Het Naardermeer voor 1883.
+					</p>
+
+
+					<img src="assets/img/naardermeer1890.jpg" />
+
+					<p class="onderschrift buildings">
+						Na het droogmaken in 1883 stopt men al in 1886 met bemalen. De opbrengsten wegen niet op tegen de kosten van het bemalen. Amsterdam wil er dan maar een vuilnisbelt van maken, maar Thijsse, Heimans & friends richten Natuurmonumenten op en kopen het gebied.
+					</p>
+
+					<img src="assets/img/naardermeer2020.jpg" />
+
+					<p class="onderschrift buildings">
+						Het meer is al decennia weer terug, maar wordt nog steeds <strong>de</strong> Naardermeer genoemd, naar de polder die het ooit was.
+					</p>
+					
+
+				</div>
+				<div class="col-md-6">
+
+					<h2>Landschapjes</h2>
+
+					<p>
+						Stadsgezichten metadateren we vaak tot op pandniveau, maar een bosgezicht is maar een 'bosgezicht'. Het is niet altijd even makkelijk, maar beter metadateren als het om locatie gaat zou inzicht geven in de veranderingen van specifieke gebieden.
+					</p>
+
+					<img src="assets/img/heide.jpg" />
+
+					<p class="onderschrift">
+						<a href="http://hdl.handle.net/10934/RM0001.COLLECT.422128">Gezicht op de heide met twee bomen</a>, Rijksmuseum. Maar welke heide?
+					</p>
+
+					<img src="assets/img/de-beer.jpg" />
+
+					<p class="onderschrift">
+						Beter beschreven: <a href="https://hdl.handle.net/21.12133/B18A3294183E4D1BA44589010AA617A2">Stuifduinen in de Beer</a>, Stadsarchief R'dam. <a href="https://nl.wikipedia.org/wiki/De_Beer_(natuurgebied)">De Beer</a> moest wijken voor de R'damse haven.
+					</p>
+
+					<p>
+						Nog beter zou het zijn als links naar Wikidata-items van afgebeelde gebieden in de metadata opgenomen zouden worden.
+					</p>
+
+					<img src="assets/img/saeftinge.jpg" />
+
+					<p class="onderschrift">
+						Met meer dan <a href="https://w.wiki/38Zv">1700 natuurlandschappen en bossen</a> is Wikidata al een serieus begin van een gezamenlijke thesaurus.
+					</p>
+					
+					
+					<h2>The Plant Collection</h2>
+					<p>
+						Bloemen in vazen te over, maar probeer eens een kamerplant op doek te vinden - vrijwel onontgonnen terrein. Naast een doek, daar vind je nog wel foto's van. Inge Meijer beschreef in <a href="https://www.stedelijk.nl/nl/evenementen/plant-collection">The Plant Collection</a> alle 39 soorten die tussen 1945 en 1983 - in het Stedelijk - op zaal naast Rothko's, Mondriaans en andere kunstwerken stonden.
+					</p>
+
+					<img src="assets/img/monstera.jpg" />
+
+					<p class="onderschrift">
+						Een monstera deliciosa op de Mondriaantentoonstelling van 1946
+					</p>
+
+					<img src="assets/img/RP-P-1902-A-22876.jpg" />
+
+					<p class="onderschrift">
+						<a href="http://hdl.handle.net/10934/RM0001.COLLECT.383979">Prent</a> van Utagawa Yoshitsuya (detail), collectie Rijksmuseum
+					</p>
+
+					
+
+					
+
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+
+					
+			<h2>Propaganda</h2>
+			<p>
+				Alleen het <a href="/helpen/index.php">IISG</a> heeft al meer dan 100.000 affiches in de collectie, veelal van allerhande 'wereldverbeteraars'. Hadden we sommige van die oproepen maar wat serieuzer genomen.
+			</p>
+
+			<img src="assets/img/vegetarisme.jpg" />
+
+			<p class="onderschrift">
+				<a href="https://hdl.handle.net/10622/97B2DA52-E37B-41DE-AF60-CFAD67FA6303">Affiche</a>, 2e kwart 20ste eeuw, collectie IISG
+			</p>
+
+			<p>
+				Op de HackaLOD2020 is een <a href="https://hicsuntleones.nl/plakmuur/iisg/index.php?topic=Vegetarianism">plakmuur</a> gemaakt waarop je de affichecollectie per topic kan bekijken.
+			</p>
+
+			<img src="assets/img/eekhoorn.jpg" />
+
+			<p class="onderschrift">
+				<a href="https://hdl.handle.net/10622/1597684D-1ADA-4F1E-A0D8-2323256945DB">Affiche</a> in de categorie 'nature conservation', collectie IISG
+			</p>
+
+			<h2>CO<sub>2</sub> & data</h2>
+
+			<p>
+				In Nederland ging <a href="https://www.cbs.nl/nl-nl/maatwerk/2020/51/elektriciteit-geleverd-aan-datacenters-2017-2019">in 2019 2,7% van alle electriciteit</a> naar datacenters. De groei is zo onstuimig, dat dat binnen een paar jaar <a href="https://www.computerworld.com/article/3431148/why-data-centres-are-the-new-frontier-in-the-fight-against-climate-change.html">wereldwijd 20%</a> zou kunnen zijn.
+			</p>
+
+			<img src="assets/img/datacenter.jpg" />
+
+			<p class="onderschrift">
+				Datacenters slurpen stroom - <a href="https://commons.wikimedia.org/wiki/File:Microsoft_Bing_Maps%27_datacenter_-_Flickr_-_Robert_Scoble.jpg">foto</a> Robert Scoble
+			</p>
+
+			<p>
+				Uitrekenen wat de CO<sub>2</sub> footprint van een archief is, lijkt me lastig. Opslag is vaak uitbesteed, energieverbruik lastig te achterhalen, en electriciteit komt in vele tinten tussen grijs en groen.
+			</p>
+
+			<p>
+				Eenvoudiger te berekenen is waarschijnlijk het aantal petabytes dat een archief op <a href="https://www.backblaze.com/blog/whats-the-diff-hot-and-cold-data-storage/">hot storage</a> heeft staan. Als elke instelling dit cijfer nou eens publiceert, om vergelijking tussen verschillende jaren en instellingen mogelijk te maken? Laten we hopen dat het dan een sport wordt dat cijfer niet te hard te laten groeien, of zelfs naar beneden te krijgen.
+			</p>
+			
+			
+
+
+		</div>
+		<div class="col-md-3">
+
+
+			<h2>Agrarische landschappenkaart</h2>
+
+			<p>
+				De RCE brengt <a href="https://www.cultureelerfgoed.nl/onderwerpen/bronnen-en-kaarten/overzicht/agrarische-landschappenkaart">in kaart</a> hoe Nederlanders door de eeuwen heen het landschap voor agrarisch gebruik in cultuur brachten.
+			</p>
+
+			<img src="assets/img/ontginningen.jpg" />
+
+			<p class="onderschrift">
+				Ontgonnen woeste gronden in Drenthe. Als je ziet hoeveel heide, veen en water is omgezet in landbouwgrond zie je dat het niet vergezocht is ook weer eens wat agrarisch gebied om te zetten in natuur.
+			</p>
+
+					
+			<h2>Lezen</h2>
+
+			<img src="assets/img/aukevanderwoud.jpg" />
+
+			<p class="onderschrift">
+				Auke van der Woud, autoriteit landschapshistorie.
+			</p>
+
+			<h2>Herbaria</h2>
+
+			<p>
+				Naturalis liet miljoenen herbariumbladen <a href="https://picturae.com/nl/ons-werk/inspirerende-projecten/hoe-miljoenen-specimen-toegankelijk-werden-voor-onderzoek">scannen</a>. Wetenschappers krijgen zo een beter beeld in verspreiding, verdwijning en opkomst van plantensoorten. Picturae kijkt inmiddels of <a href="https://picturae.com/nl/wat-we-doen/collecties-digitaliseren/insecten">insecten</a> zo ook te digitaliseren zijn.
+			</p>
+
+			<img src="assets/img/korenbloemen.jpg" />
+
+			<p class="onderschrift">
+				<a href="https://bioportal.naturalis.nl/result/multimedia/term=korenbloem&size=100&from=0">Enkele van de honderden korenbloemen</a> in de collectie van Naturalis.
+			</p>
+
+			<!--
+			<img src="assets/img/krznaric.jpg" />
+
+			<p class="onderschrift">
+				Krznaric kijkt zeven generaties vooruit, maar verder is langetermijndenken erfgoedmensen niet vreemd. 
+			</p>
+
+			<img src="assets/img/hickel.jpg" />
+
+			<p class="onderschrift">
+				Kapitalisme is onderdeel van het probleem. Met historisch overzicht.
+			</p>
+
+			<img src="assets/img/ponting.jpg" />
+
+			<p class="onderschrift">
+				Een ouwetje uit de vorige eeuw. Toen voor veel mensen een eyeopener - milieuproblemen bleken beschavingen te kunnen nekken.
+			</p>
+			
+			<h2>Reizen</h2>
+
+			<h2>Postkapitalisme</h2>
+			
+			<h2>plezier vroeger</h2>
+
+			-->
+
+			
+
+
+		</div>
+	</div>
+	
+</div>
