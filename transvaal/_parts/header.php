40437a34e817f2177ec2342f312bc49bede89cb8
Menno den Engelse <menno@denengelse.nl>
2026-01-23 13:25:09 +0100

    added texts

--- transvaal/_parts/header.php
+++ transvaal/_parts/header.php
@@ -27,7 +27,7 @@
 </head>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
+    <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Bioboost Transvaal</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- transvaal/index.php
+++ transvaal/index.php
@@ -13,18 +13,55 @@ include("_parts/header.php");
 	
 </div>
 
+<div class="container-fluid" id="hometxt">
 
-<div id="contentbox">
-	
-	<h1>Bioboost Transvaal</h1>
+	<div class="row">
 
-	<p>De Haarlemse Transvaalwijk is bepaald geen groene wijk. De Generaalsbuurt staat zelfs in de top drie van meest versteende buurten van Nederland.</p>
+		<div class="col-md-3">
 
-	<p>Toch blijkt: ook hier vinden planten tussen stoeptegels een huis, weten vlinders de bloemen tussen al het steen te vinden. De woestijn leeft!</p>
+			<h2>Transvaal</h2>
 
-	<p>In 2026 proberen we een jaar lang zoveel mogelijk soorten in de wijk te ontdekken. Onze aan iNaturalist toegevoegde waarnemingen verschijnen binnen een paar dagen vanzelf op deze kaart.</p>
-		
+			
+			<p>De Haarlemse Transvaalwijk is bepaald geen groene wijk. De Generaalsbuurt staat zelfs in de top drie van meest versteende buurten van Nederland.</p>
+
+			<p>Toch blijkt: ook hier vinden planten tussen stoeptegels een huis, weten vlinders de bloemen tussen al het steen te vinden. De woestijn leeft!</p>
+
+		</div>
+
+		<div class="col-md-3">
+			
+			
+			<h2>Bioboost</h2>
+			
+			<p>We gaan de biodiversiteit een handje helpen door voor meer onderkomens, leefgebied en voedsel te zorgen. Vogelhuisjes maken bijvoorbeeld, of insectenhotels. Elke tegel eruit is winst: een klein geveltuintje kan al tientallen soorten planten en insecten huisvesten.</p>
+
+		</div>
+
+		<div class="col-md-3">
+
+
+			<h2>Big Year 2026</h2>
+			
+			<p>In 2026 proberen we een jaar lang zoveel mogelijk soorten in de wijk te ontdekken. Onze aan iNaturalist toegevoegde waarnemingen verschijnen binnen een paar dagen vanzelf op deze kaart.</p>
+
+
+		</div>
+
+		<div class="col-md-3">
+			
+			
+			<h2>Meedoen?</h2>
+			
+			<p>Je kunt wat meer (inheemse) planten in je tuin zetten om vlinders plek te bieden of een insectenhotel plaatsen. Woon je op een bovenwoning? Zelfs dan kan je insecten helpen met wat planten op je balkon. Of je helpt mee door met iNaturalist de biodiversiteit in kaart te brengen.</p>
+
+			<p>Alle mogelijkheden om mee te doen vind je op de <a href="meedoen.php">Meedoen?</a> pagina.</p>
+
+		</div>
+
+	</div>
+
 </div>
+
 
 
 <div id="searchresults">
