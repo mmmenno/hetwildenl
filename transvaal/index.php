bc6a02355f4323bd12e578e976881bdff1df30c4
Menno den Engelse <menno@denengelse.nl>
2026-01-29 09:47:09 +0100

    textual changes

--- transvaal/index.php
+++ transvaal/index.php
@@ -34,7 +34,7 @@ include("_parts/header.php");
 			
 			<p>De Haarlemse Transvaalwijk is bepaald geen groene wijk. De Generaalsbuurt staat zelfs in de top drie van meest versteende buurten van Nederland.</p>
 
-			<p>Toch blijkt: ook hier vinden planten tussen stoeptegels een huis, weten vlinders de bloemen tussen al het steen te vinden. De woestijn leeft (als je goed kijkt).</p>
+			<p>En toch, ook hier vinden planten tussen stoeptegels een huis, weten vlinders de bloemen tussen al het steen te vinden. De woestijn leeft (als je goed kijkt).</p>
 
 		</div>
 
@@ -43,7 +43,9 @@ include("_parts/header.php");
 			
 			<h2>Bioboost</h2>
 			
-			<p>We gaan de biodiversiteit een handje helpen door voor meer onderkomens, leefgebied en voedsel te zorgen. Vogelhuisjes maken bijvoorbeeld, of insectenhotels. Elke tegel eruit is winst: een klein geveltuintje kan al tientallen soorten planten en insecten huisvesten.</p>
+			<p>We willen het beetje natuur in de wijk een handje helpen. Dat kan door planten meer ruimte tussen het steen te bieden om te wortelen (wip die tegels!) en zo meer voedsel en verblijfplekken te bieden aan vlinders, vogels en andere dieren. Een klein geveltuintje kan al tientallen soorten planten en insecten huisvesten. Met broedkasten en insectenhotels maken we de buurt ook wat aanlokkelijker.</p>
+
+			<p>Dat kunnen we niet alleen. We hopen, door te laten zien wat er allemaal leeft, medebuurtbewoners zo enthousiast te krijgen dat ze ook de handen uit de mouwen steken. Of juist wat minder actief 'onkruid' verwijderen en rommelhoekjes opruimen.</p>
 
 		</div>
 
@@ -51,8 +53,12 @@ include("_parts/header.php");
 
 
 			<h2>2026: Big Year</h2>
+
+			<p>Vogelaars kunnen nogal competitief zijn als het om lijstjes gaat. Dat resulteert soms in een <a href="https://en.wikipedia.org/wiki/Big_year">Big Year</a>, een jaar waarin ze op onmogelijke tijden opstaan en ongegeneerd fossiele brandstoffen verbranden om in een bepaald gebied (dit kan de hele wereld zijn) zoveel mogelijk vogelsoorten af te vinken.</p>
 			
-			<p>In 2026 proberen we een jaar lang zoveel mogelijk soorten in de wijk te ontdekken. Onze aan iNaturalist toegevoegde waarnemingen verschijnen binnen een paar dagen vanzelf op deze kaart.</p>
+			<p>Ons Big Year houden we binnen de grenzen van de wijk. We beperken ons niet tot vogels. En we doen het samen.</p>
+
+			<p>Iedereen kan meedoen: we gebruiken de app van <a href="https://www.inaturalist.org/">iNaturalist</a> om waarnemingen te verzamelen. Die verschijnen hierboven op de kaart en op de pagina <a href="soorten.php">soorten</a>. Af en toe gaan we op buurtsafari, waarin we samen op zoek gaan naar planten en dieren. Hou daarvoor de <a href="agenda.php">agenda</a> in de gaten. Tijdens safari's kunnen we je helpen met de app.</p>
 
 
 		</div>
@@ -61,10 +67,12 @@ include("_parts/header.php");
 			
 			
 			<h2>Meedoen?</h2>
-			
-			<p>Je kunt wat meer (inheemse) planten in je tuin zetten om vlinders plek te bieden of een insectenhotel plaatsen. Woon je op een bovenwoning? Zelfs dan kan je insecten helpen met wat planten op je balkon. Of je helpt mee door met iNaturalist de biodiversiteit in kaart te brengen.</p>
 
-			<p>Alle mogelijkheden om mee te doen vind je op de <a href="meedoen.php">Meedoen?</a> pagina.</p>
+			<p>Er zijn allerlei manieren waarop je kunt helpen, we hebben ze <a href="meedoen.php">hier</a> op een rij gezet.</p>
+			
+			<p>Of je nou een hotelletje voor metselbijen ophangt, een geveltuintje aanlegt, af en toe de vogels wat bijvoert of voor wat meer inheemse planten zorgt, de niet-menselijke buurtbewoners zullen je dankbaar zijn!</p>
+
+			<p>Het begint allemaal met goed om je heen te kijken en het leven om je heen te <em>zien</em>.</p>
 
 		</div>
 
