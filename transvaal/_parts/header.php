81398d6cf89ab68b7db6aade123d445dcbb6a332
Menno den Engelse <menno@denengelse.nl>
2026-02-07 09:01:05 +0100

    added plausible

--- transvaal/_parts/header.php
+++ transvaal/_parts/header.php
@@ -22,7 +22,13 @@
 	<link rel="stylesheet" href="_assets/css/styles.css" />
 
 	
-	<script defer data-domain="hetwilde.nl" src="https://plausible.io/js/script.js"></script>
+	<!-- Privacy-friendly analytics by Plausible -->
+	<script async src="https://plausible.io/js/pa-eOF38_hcvjNb2uiPVBUuu.js"></script>
+	<script>
+	  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+	  plausible.init()
+	</script>
+
 	
 </head>
 
