b0c7b30b52b4ca07db5b011b29bec0bb3fc0fa12
Menno den Engelse <menno@denengelse.nl>
2026-02-24 07:48:07 +0100

    eerste agenda item

--- transvaal/agenda.php
+++ transvaal/agenda.php
@@ -28,20 +28,24 @@ include("_parts/header.php");
 
 	</div>
 
-	<div class="row">
+	<div class="row agenda-item">
 
 		<div class="col-md-4">
 			
-				
+			<h2>Zondag 1 maart: eerste buurtsafari</h2>
 
-				
+			<p>We gaan dit jaar een aantal buurtsafari's doen. Onder leiding van iemand met kennis van zaken gaan we dan in Transvaal op zoek naar wat er zoal leeft. Dat kunnen planten zijn, maar ook bijvoorbeeld vleermuizen of insecten. We gebruiken de app iNaturalist om waarnemingen vast te leggen (onder <a href="meedoen.php">Meedoen</a> vind je linkjes naar dat platform en uitleg).</p>
+
+			<p>Zondag 1 maart gaan we oefenen. We starten om 11:30 op het Pretoriaplein. We gaan kijken hoe de iNaturalist app werkt en hoe je er planten en beestjes mee determineert.</p>
+
+			<p>Voorafgaand aan de buurtsafari werken we, vanaf 10:00 uur, mee met het groenonderhoudsploegje van het Pretoriaplein. Als je wel zin hebt om de handen een beetje uit de mouwen te steken kun je dus ook al iets eerder komen.</p>
 
 
 		</div>
 
 		<div class="col-md-4">
 			
-				
+			<img src="_assets/img/groenonderhoud-1-maart.png" />
 
 				
 
